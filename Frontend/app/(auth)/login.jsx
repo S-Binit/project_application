c6efef5d846eb56eebe5171b4a95f8e45cf78c4b
Binit Shrestha <bnitshrestha078@gmail.com>
2025-12-18 23:36:13 +0545

    added pressable components, login and register buttons

--- Frontend/app/(auth)/login.jsx
+++ Frontend/app/(auth)/login.jsx
@@ -1,14 +1,21 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Pressable, Text } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 import React from 'react'
 
 //themed components
 import ThemedText from '../../components/ThemedText'
 import ThemedView from '../../components/ThemedView'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 
-const login = () => {
+const Login = () => {
+
+    const handleSubmit = () => {
+        console.log('login form submitted')
+    }
+
   return (
     <ThemedView style={styles.container}>
 
@@ -16,6 +23,10 @@ const login = () => {
     <ThemedText title={true} style={styles.title}>
         Login to Your Account
     </ThemedText>
+
+    <ThemedButton onPress={handleSubmit}>
+        <Text style={{color: '#f2f2f2'}}>Login</Text>
+    </ThemedButton>
 
     <Spacer height={100}/>
     <Link href='/register'>
@@ -28,16 +39,25 @@ const login = () => {
   )
 }
 
-export default login
+export default Login
 
 const styles = StyleSheet.create({
     container:{
         flex: 1,
         justifyContent: "center",
+        alignItems: 'center',
     },
     title: {
         textAlign: "center",
         fontSize: 18,
         marginBottom: 30
     },
+    btn: {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5,
+    },
+    pressed:{
+        opacity: 0.8
+    }
 })
